79d42d2a750ac3eb2c3e756bbcbdd8b1fa9f9d71
Norman Vasquez <nrvasquez41@hotmail.com>
2020-09-18 17:38:51 -0600

    configuring eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,17 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module'
   },
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
-  rules: {}
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'error'
+  }
 };
