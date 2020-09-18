b0acdd96d1c7e56db2c6d3adcebe9157b386aa90
Norman Vasquez <nrvasquez41@hotmail.com>
2020-09-18 17:37:53 -0600

    changing from http to https

--- server-for-browser/refresh.sh
+++ server-for-browser/refresh.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/entrevistas.xml https://cienciaes.com/feed/entrevistas
-http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/vanguardia.xml https://cienciaes.com/feed/vanguardia
-http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/cienciafresca.xml https://cienciaes.com/feed/cienciafresca
-http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/ciertaciencia.xml https://cienciaes.com/feed/ciertaciencia
-http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/neutrino.xml https://cienciaes.com/feed/neutrino
-http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/extrema.xml https://cienciaes.com/feed/extrema
-http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/quilociencia.xml https://cienciaes.com/feed/quilociencia
-http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/oceanos.xml https://cienciaes.com/feed/oceanos
-http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/seispatas.xml https://cienciaes.com/feed/seispatas
-http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/fosiles.xml https://cienciaes.com/feed/fosiles
-http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/ciencianuestra.xml https://cienciaes.com/feed/ciencianuestra
-http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/biografias.xml https://cienciaes.com/feed/biografias
-http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/ulises.xml https://cienciaes.com/feed/ulises
-http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/feed.xml https://cienciaes.com/feed
+http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/entrevistas.xml http://cienciaes.com/feed/entrevistas
+http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/vanguardia.xml http://cienciaes.com/feed/vanguardia
+http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/cienciafresca.xml http://cienciaes.com/feed/cienciafresca
+http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/ciertaciencia.xml http://cienciaes.com/feed/ciertaciencia
+http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/neutrino.xml http://cienciaes.com/feed/neutrino
+http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/extrema.xml http://cienciaes.com/feed/extrema
+http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/quilociencia.xml http://cienciaes.com/feed/quilociencia
+http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/oceanos.xml http://cienciaes.com/feed/oceanos
+http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/seispatas.xml http://cienciaes.com/feed/seispatas
+http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/fosiles.xml http://cienciaes.com/feed/fosiles
+http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/ciencianuestra.xml http://cienciaes.com/feed/ciencianuestra
+http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/biografias.xml http://cienciaes.com/feed/biografias
+http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/ulises.xml http://cienciaes.com/feed/ulises
+http --download --output /home/nvasquez/Documents/github/cienciaes/server-for-browser/data/feed.xml http://cienciaes.com/feed
