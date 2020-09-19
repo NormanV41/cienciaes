480d51a6a5f046845a14436a3fe90ab3a055fbca
Norman Vasquez <nrvasquez41@hotmail.com>
2020-09-19 13:57:57 -0600

    trying to add requiring type checking

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,17 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module'
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json', './tsconfig.app.json', './tsconfig.eslint.json', './tsconfig.spec.json']
   },
   plugins: ['@typescript-eslint', 'prettier'],
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
